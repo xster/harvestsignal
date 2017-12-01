e20990400ec9eb78eba1597ce24b06c496198bdc
Xiao Yu <xster@google.com>
2017-12-01 01:04:16 -0800

    font, location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(
   new MaterialApp(
@@ -11,20 +12,45 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  bool loading = true;
+  Map<String,double> location;
+
+  @override
+  void initState() {
+    super.initState();
+    new Location().getLocation.then((Map<String,double> location) {
+      setState(() {
+        this.location = location;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new ListView(
-      children: <Widget>[
-        const Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 24.0),
-          child: const Text(
-            'Harvest',
-            style: const TextStyle(
-              fontFamily: 'Nunito',
-              fontSize: 32.0))),
-      ],
+    List<Widget> stackChildren = <Widget>[];
+    if (location == null) {
+      stackChildren.add(new Center(child: new Image.asset('assets/load.gif')));
+    }
+
+    stackChildren.add(
+      new ListView(
+        children: <Widget>[
+          const Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 24.0),
+            child: const Text(
+              'Harvest',
+              style: const TextStyle(
+                fontFamily: 'Nunito',
+                fontSize: 48.0,
+                color: const Color(0xFF222222),
+                ))),
+          new Text(location?.toString()),
+        ],
+      ),
     );
+
+    return new Stack(children: stackChildren);
   }
 }
