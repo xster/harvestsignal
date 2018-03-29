98631cd6da294dbfe68007580e1d023e64943f67
Xiao Yu <xiao@xster.net>
2018-03-28 23:14:35 -0700

    First round

---
+++ lib/api.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:google_maps_webservice/geocoding.dart';
+import 'package:http/http.dart' as http;
+import 'package:location/location.dart' as gps;
+
+Future<String> getZipCode() async {
+  final Map<String, double> currentLocation = await new gps.Location().getLocation;
+  final GeocodingResponse geocodingResponse =
+      await new GoogleMapsGeocoding(null).searchByLocation(
+        new Location(currentLocation['latitude'], currentLocation['longitude'])
+      );
+  if (geocodingResponse.status == GoogleResponseStatus.okay) {
+    return geocodingResponse.results[0].addressComponents.singleWhere(
+      (AddressComponent component) {
+        return component.types.contains('postal_code');
+      }
+    ).shortName;
+  } else {
+    return null;
+  }
+}
+
+Future<List<Map<String, dynamic>>> getProduce(String zipCode) async {
+  final http.Response response = await http.get(
+    'https://harvestsignal.com/api/v1/public/whatsfresh/$zipCode'
+  );
+
+  if (response.statusCode != 200) {
+    return null;
+  }
+
+  final Map<String, List<dynamic>> rootJson = JSON.decode(response.body);
+  return rootJson['data'];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_webservice/geocoding.dart';
-import 'package:location/location.dart' as gps;
+
+import 'api.dart';
 
 void main() => runApp(
   new MaterialApp(
     home: new Scaffold(
-      body: new Home())));
+      body: new DefaultTextStyle(
+        style: const TextStyle(
+          fontFamily: 'Nunito'),
+        child: new Home()))));
 
 class Home extends StatefulWidget {
   @override
@@ -14,18 +17,16 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   bool loading = true;
-  Map<String,double> location;
+  List<Map<String, dynamic>> produce;
 
   @override
   void initState() {
     super.initState();
-    new gps.Location().getLocation.then((Map<String,double> location) {
-      new GoogleMapsGeocoding(null).searchByLocation(
-        new Location(location['latitude'], location['longitude'])
-      ).then((GeocodingResponse response) {
-        if (response.status == GoogleResponseStatus.okay) {
-          print(response.results[0].addressComponents);
-        }
+    getZipCode().then((String zipCode) {
+      getProduce(zipCode).then((List<Map<String, dynamic>> result) {
+        setState(() {
+          produce = result;
+        });
       });
     });
   }
@@ -33,25 +34,60 @@ class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     List<Widget> stackChildren = <Widget>[];
-    if (location == null) {
+    if (produce == null) {
       stackChildren.add(new Center(child: new Image.asset('assets/load.gif')));
     }
 
+    List<Widget> items = <Widget> [new SliverPersistentHeader(
+      delegate: new SliverPersistentHeaderDelegate(
+
+      ),
+    )
+    SliverAppBar(
+      backgroundColor: Colors.white,
+
+      title: const Text(
+        'Harvest',
+        style: const TextStyle(
+          fontFamily: 'Nunito',
+          fontSize: 48.0,
+          color: const Color(0xFF222222),
+        ),
+      ),
+    )];
+
+    if (produce != null) {
+      items.add(
+        new SliverGrid.count(
+          crossAxisCount: 2,
+          children: produce.map((Map<String, dynamic> item) {
+            return new Card(
+              elevation: 10.0,
+              child: new Stack(
+                children: <Widget>[
+                  new Image.network('https://harvestsignal.com/assets/produce-pictures/${item['slug']}.jpg'),
+                  new Align(
+                    alignment: FractionalOffset.topLeft,
+                    child: new Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 6.0,
+                      ),
+                      color: const Color(0x77FFFFFF),
+                      child: new Text(item['produce_name'])
+                    ),
+                  ),
+                ]
+              ),
+            );
+          }).toList(),
+        ),
+      );
+    }
+
     stackChildren.add(
-      new ListView(
-        children: <Widget>[
-          const Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 24.0),
-            child: const Text(
-              'Harvest',
-              style: const TextStyle(
-                fontFamily: 'Nunito',
-                fontSize: 48.0,
-                color: const Color(0xFF222222),
-                ))),
-        ],
+      new CustomScrollView(
+        slivers: items,
       ),
     );
 
